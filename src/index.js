24ec656331faef131261f709a52d383870392826
Ilya <gign.brest@gmail.com>
2020-10-26 00:17:57 +0300

    feat: all test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
+  module.exports = function reverse (n) {
+    let f='';
+    let nn=Math.abs(n);
+      while (nn>0) {
+          f+=nn%10;
+          nn=(nn-nn%10)/10;
   
+    }
+    return f;
 }
